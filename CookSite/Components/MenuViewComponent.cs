2a368b56c355aea449d875d40a60b7f0d5711860
KHMYO <haltasodev@gmail.com>
2025-05-20 10:17:26 +0300

    menu view component added

---
+++ CookSite/Components/MenuViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using CookSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookSite.Components
+{
+    public class MenuViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            CookSiteContext db = new CookSiteContext();
+            var cookTypes = db.CookTypes.ToList();
+            return View(cookTypes);
+        }
+
+    }
+}
